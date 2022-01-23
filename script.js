83e901be17ea0cfbea2090eb03c7cac7c8a13293
Varenya19 <97852558+Varenya19@users.noreply.github.com>
2022-01-23 09:29:43 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    let principal = document.getElementById('principal').value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+    principal = Number(principal);
+    rate = Number(rate);
+    years = Number(years);
+
+    if (isNaN(principal)) {
+        alert('Sorry, we only accept numbers. Can you please enter a number for the value of Amount.');
+        return;
+    }
+
+    if (isNaN(rate)) {
+        alert('Sorry, we only accept numbers. Can you please enter a number for the value of Interest rate.');
+        return;
+
+    }
+    if (isNaN(years)) {
+        alert('Sorry, we only accept numbers. Can you please enter a number for the value of No. of Years.');
+        return;
+    }
+
+    const recieveAmount = principal * rate * years;
+    const recieveYear = new Date().getFullYear() + years;
+
+    document.getElementById('deposit').innerText = principal;
+    document.getElementById('interest').innerText = rate;
+    document.getElementById('recieveAmount').innerText = recieveAmount;
+    document.getElementById('receiveYear').innerText = recieveYear;
+    document.getElementById('messageDiv').classList.remove('none');
 }
-        
+function onSliderChange(event) {
+    const sliderValue = event.target.value;
+    document.getElementById('sliderValue').innerText = sliderValue + '%';
+}
